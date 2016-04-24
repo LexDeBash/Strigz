6a5ecd2fcfce92a5e14a40e4aca4e1524c241e7f
Alexey Efiomv <lex.efimov@gmail.com>
2016-04-24 16:52:51 +0500

    Включение обработки ошибок

--- Strigz/main.m
+++ Strigz/main.m
@@ -14,11 +14,26 @@ int main(int argc, const char * argv[]) {
         for (int i = 0; i < 10; i++) {
             [str appendString:@"Aaron is cool!\n"];
         }
-        [str writeToFile:@"/tmp/cool.txt"
-              atomically:YES
-                encoding:NSUTF8StringEncoding
-                   error:NULL];
-        NSLog(@"done writing /tmp/cool.txt");
+        
+        // Здесь указатель на объект NSError объявляется без создания
+        // экземпляра. Экземпляр NSError будет создан только при возникновении ошибки.
+        NSError *error = nil;
+        
+        // Передача по ссылке указателя NSError методу NSString
+        
+        BOOL success = [str writeToFile:@"/tmp/cool.txt"
+                             atomically:YES
+                               encoding:NSUTF8StringEncoding
+                                  error:&error];
+        
+        // Проверка полученного флага и вывод информации
+        // из NSError в случае ошибки записи
+        if (success) {
+            NSLog(@"done writing /tmp/cool.txt");
+        } else {
+            NSLog(@"writing /tmp/cool.txt failed: %@", [error localizedDescription]);
+        }
+        
     }
     return 0;
 }
